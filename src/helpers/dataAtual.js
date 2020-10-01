4daa6a6ce99389efdf0d6b99e00f083f72158978
ladeirakelwin <ladeirakelwin@gmail.com>
2020-10-01 12:09:09 -0300

    modificando dataAtual()

--- src/helpers/dataAtual.js
+++ src/helpers/dataAtual.js
@@ -1,4 +1,5 @@
 module.exports = () => {
-  const data = new Date();
-  return (`${data.getDate()}-${data.getMonth()+1}-${data.getFullYear()} ${data.getHours()}:${data.getMinutes()}:${data.getSeconds()}:${data.getMilliseconds()}`);
+  return Date.now();
 };
+
+// (`${data.getDate()}-${data.getMonth()+1}-${data.getFullYear()} ${data.getHours()}:${data.getMinutes()}:${data.getSeconds()}:${data.getMilliseconds()}`)
