2d074d2f30cb4be39da0142e12ceafb3751d62c2
ladeirakelwin <ladeirakelwin@gmail.com>
2020-10-03 15:43:28 -0300

    Refatorando deleta produtos

--- src/controllers/controller-products.js
+++ src/controllers/controller-products.js
@@ -1,59 +1,49 @@
-require('dotenv/config');
-const mongoose = require('mongoose');
+require("dotenv/config");
+const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
-const Produto = require('../models/model-product');
-const Fornecedor = require('../models/model-provider');
-const exibirDados = require('../helpers/exibirDados');
-const exibirProduto = require('../helpers/exibirProduto');
-const atualizaProduto = require('../helpers/atualizaProduto');
-const deletaProdutos = require('../helpers/deletaProdutos');
-const insereProdutos = require('../helpers/insereProdutos');
-
-
+const Produto = require("../models/model-product");
+const Fornecedor = require("../models/model-provider");
+const exibirDados = require("../helpers/exibirDados");
+const exibirProduto = require("../helpers/exibirProduto");
+const atualizaProduto = require("../helpers/atualizaProduto");
+const deletaProdutos = require("../helpers/deletaProdutos");
+const insereProdutos = require("../helpers/insereProdutos");
 
 class ProdutoController {
+  static exibeProdutos() {
+    return (req, res) => {
+      exibirDados(Produto, res);
+    };
+  }
 
-    static exibeProdutos() {
+  static exibeProduto() {
+    return (req, res) => {
+      exibirDado(Produto, req, res);
+    };
+  }
 
-        return (req, res) => {
-            exibirDados(Produto, res);
+  static deletaProduto() {
+    return async (req, res) => {
+      await deletaProdutos(Produto, req, res);
+    };
+  }
 
-        }
-    }
+  static adicionaProduto() {
+    return async (req, res) => {
+      const produto = new Produto( {...req.body} );
+      produto.save((err) => { if(err) res.send(JSON.stringify({erro: err}))})
+      res.redirect('/produto');
+    };
+  }
 
-    static exibeProduto() {
-
-        return (req, res) => {
-            exibirProduto(Produto, req ,res);
-
-        }
-    }
-
-    static deletaProdutos() {
-    
-
-        return async (req, res) => {            
-            await deletaProdutos(Produto, req, res)
-        }
-    }
-
-    static addProdutos() {
-
-        return async (req, res) => {
-            await insereProdutos(Produto, Fornecedor, req, res);
-        }
-    }
-
-    static atualizaProdutos(){
-
-        return (req, res) => {
-            atualizaProduto(Produto, req.params.id, req.body, res);
-        }
-
-    }
+  static atualizaProduto() {
+    return (req, res) => {
+      atualizaProduto(Produto, req.params.id, req.body, res);
+    };
+  }
 }
 
 module.exports = ProdutoController;
