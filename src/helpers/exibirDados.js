a18502ea38b2b192723f45b7d8aa101b66685cd8
ladeirakelwin <ladeirakelwin@gmail.com>
2020-10-03 14:58:36 -0300

    Refatorando exibirDados

--- src/helpers/exibirDados.js
+++ src/helpers/exibirDados.js
@@ -1,5 +1,5 @@
-module.exports = function exibirDados(Model,res){
-    Model.find({}, (err, elemento)=>{
+module.exports = (Model,res) => {
+    Model.find({}, (err, elemento) => {
         if (err) res.send(JSON.stringify({results: err}));
         res.send(JSON.stringify({results: elemento}));
     });
