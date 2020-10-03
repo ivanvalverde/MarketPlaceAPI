caa9508fb2f8aefa733d9a4ecb061c684bdddb99
ladeirakelwin <ladeirakelwin@gmail.com>
2020-10-03 16:29:28 -0300

    Removendo tentativa de alteração de fornecedor

--- src/helpers/atualizaDados.js
+++ src/helpers/atualizaDados.js
@@ -1,6 +1,7 @@
 module.exports = async (Model, req, res) => {
   const { _id } = req.params;
   delete req.body.senha;
+  delete req.body.fornecedor;
   try {
     await Model.updateOne({ _id }, { ...req.body });
     res.send(JSON.stringify({ modified: true }));
