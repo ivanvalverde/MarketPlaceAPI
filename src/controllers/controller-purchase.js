2474a047cb5095d89ae08c7ddb0b02b0ffa030bc
ladeirakelwin <ladeirakelwin@gmail.com>
2020-10-03 19:14:25 -0300

    Refatorando realização de compra

--- src/controllers/controller-purchase.js
+++ src/controllers/controller-purchase.js
@@ -1,73 +1,67 @@
-require('dotenv/config');
-const mongoose = require('mongoose');
+require("dotenv/config");
+const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-const Compra = require('../models/model-purchase');
-const exibirDados = require('../helpers/exibirDados');
-const atualizaDados = require('../helpers/atualizaDados');
-const insereDados = require('../helpers/insereDados');
-const deletaDados = require('../helpers/deletaDados');
-const dataAtual = require('../helpers/dataAtual');
-
+const Compra = require("../models/model-purchase");
+const Cliente = require("../models/model-customer");
+const Produto = require("../models/model-product");
+const exibirDados = require("../helpers/exibirDados");
+const atualizaDados = require("../helpers/atualizaDados");
+const insereDados = require("../helpers/insereDados");
+const deletaDados = require("../helpers/deletaDados");
+const dataAtual = require("../helpers/dataAtual");
+const verificaExistencia = require("../helpers/verificaExistencia");
+const Usuario = require("../models/model-customer");
 
 class CompraController {
+  static exibeCompras() {
+    return (req, res) => {
+      exibirDados(Compra, res);
+    };
+  }
 
-    static exibeCompras() {
+  static exibeComprasProduto() {
+    return (req, res) => {
+      const { idProduto } = req.params;
+      Compra.find({ idProduto }, (err, compras) => {
+        if (err) res.send(JSON.stringify({ results: err }));
+        res.send(JSON.stringify({ results: elemento }));
+      });
+    };
+  }
 
-        return (req, res) => {
+  static exibeComprasCliente() {
+    return (req, res) => {
+      const { idCliente } = req.params;
+      Compra.find({ idCliente }, (err, compras) => {
+        if (err) res.send(err);
+        res.send(compras);
+      });
+    };
+  }
 
-            exibirDados(Compra, res);
+  static cancelaCompra() {
+    return async (req, res) => {
+      const { _id } = req.params;
+      await Compra.update({ _id }, { $set: { dataCancelamento: dataAtual() } });
+      res.send("Item Modificado");
+    };
+  }
 
-        }
-    }
+  static realizaCompra() {
+    return async (req, res) => {
+      const { idProduto, idCliente } = req.body;
+      verificaExistencia(Cliente, idCliente, res);
+      verificaExistencia(Produto, idProduto, res);
+      const compra = new Compra({idProduto, idCliente, dataCompra: dataAtual(),dataCancelamento: null});
 
-    static exibeComprasProduto() {
-
-        return (req, res) => {
-            const { idProduto } = req.params;
-            Compra.find({idProduto}, (err, compras) =>{
-                if(err) res.send(err)
-                res.send(compras)
-            })
-
-        }
-    }
-
-    static exibeComprasCliente() {
-
-        return (req, res) => {
-            const { idCliente } = req.params;
-            Compra.find({idCliente}, (err, compras) =>{
-                if(err) res.send(err)
-                res.send(compras)
-            })
-
-        }
-    }
-
-    static cancelaCompra() {
+      compra.save((err) => { if (err) res.send(err); });
       
-
-        return async (req, res) => {
-            const { _id } = req.params;
-            await Compra.update({ _id },{$set: { dataCancelamento: dataAtual()}})
-            res.send("Item Modificado")
-        }
-    }
-
-    static realizaCompra() {
-
-        return async (req, res) => {
-            const {idProduto, idCliente} = req.body;
-            const compra = new Compra({idProduto, idCliente, dataCompra: dataAtual(), dataCancelamento: null})
-            await compra.save((err) => {
-                if(err) res.send(err)
-            })
-            res.redirect('/compra')
-        }
-    }
+      res.redirect("/compra");
+    };
+  }
 }
 
 module.exports = CompraController;
