f5a33c7d1ce73fed11e0ea092dc21480ae5f97b4
ladeirakelwin <ladeirakelwin@gmail.com>
2020-10-03 21:16:24 -0300

    Refatorando fornecedor

--- src/controllers/fornecedores.js
+++ src/controllers/fornecedores.js
@@ -1,57 +1,49 @@
-const banco = require('../config/database');
-const Fornecedor = require('../models/fornecedor');
-const exibirDados = require('../helpers/exibirDados');
-const exibirDado = require('../helpers/exibirDado');
-const atualizaDados = require('../helpers/atualizaDados');
-const insereDados = require('../helpers/insereDados');
-const deletaDados = require('../helpers/deletaDados');
-
+const banco = require("../config/database");
+const Fornecedor = require("../models/fornecedor");
+const exibirDados = require("../helpers/exibirDados");
+const exibirDado = require("../helpers/exibirDado");
+const atualizaDados = require("../helpers/atualizaDados");
+const insereDados = require("../helpers/insereDados");
+const deletaDados = require("../helpers/deletaDados");
 
 class FornecedorController {
-
   static exibeFornecedores() {
-
     return (req, res) => {
       exibirDados(Fornecedor, res);
-    }
+    };
   }
 
   static exibeFornecedor() {
-
     return (req, res) => {
       exibirDado(Fornecedor, req, res);
-    }
+    };
   }
 
   static deletaFornecedor() {
-
     return (req, res) => {
-      deletaDados(Fornecedor, req, res)
-    }
+      deletaDados(Fornecedor, req, res);
+    };
   }
   static adicionaFornecedor() {
-
     return (req, res) => {
-      const { razaoSocial, cnpj, senha } = req.body; 
+      const { razaoSocial, cnpj, senha } = req.body;
       const fornecedor = insereDados(Fornecedor, req);
       fornecedor.cnpj = cnpj;
       fornecedor.razaoSocial = razaoSocial;
       fornecedor.setSenha(senha);
-      res.send(JSON.stringify({results: fornecedor}))
-    
-      fornecedor.save((err) =>  
-      { if(err) res.send(JSON.stringify({ erro:"Não foi posível salvar."})) }
-      );
 
-      res.redirect('/fornecedor')
-    }
+      fornecedor.save((err) => {
+        if (err) res.send(JSON.stringify({ erro: "Não foi posível salvar." }));
+      });
+
+      res.redirect("/fornecedor");
+    };
   }
 
   static atualizaFornecedor() {
-
     return (req, res) => {
       atualizaDados(Fornecedor, req, res);
-    }
+    };
   }
 }
 
